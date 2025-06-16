9338ed8ebf44b9c7bab74d7e1e8594ebbebd820d
QuantumSafeio <QuantumSafeacode@gmail.com>
2025-06-16 07:15:49 +0300

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+الاستيراد React from 'react'
+import ReactDOM from الاستيراد  
+الاستيراد App التطبيق './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+رد....createRoot.....document.....getElementById..............'root'.....).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  <رد...
 )
