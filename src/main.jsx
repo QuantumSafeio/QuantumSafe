f7313dfac584339ba7a6b52b0e0293cd94fce448
QuantumSafeio <QuantumSafeacode@gmail.com>
2025-06-16 07:15:23 +0300

    Fix Arabic syntax errors in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import رد فعل  DOM فعل DOM from "رد فعل دوم / عميل"
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-رد فعل DOMفعل  DOM.createRoot(document.الحصول على الحصول على ElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </رد  فعل. فعل..StrictMode>
+  </React.StrictMode>
 )
